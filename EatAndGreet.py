935375ff15dddf109c45d333c4dca5988bc6ddb3
passwordlandia2 <jamiller8686@outlook.com>
2024-09-12 16:34:15 -0700

    Add files via upload
    This is my fun take on a basic hello world python script with a way for me to understand how naming the variable the same thing post "print" action, leads to a different output and overwrites the existing information but not always, just when it's set up that way. It was my first Python script and I had fun with it.

---
+++ EatAndGreet.py
@@ -0,0 +1,37 @@
+# A simple greeting just for fun, but practicing syntax.
+print("")
+hello = "Hello!"
+print(hello)
+hello = "My Name Is Ignego Montoya!"
+print(hello)
+hello = "You Killed My Father!"
+print(hello)
+hello = "Prepare to Die!"
+print(hello)
+print("")
+# I've created and Introduction to the contents.
+
+intro = "What did I eat today you ask?"
+print(intro)
+print("")
+# We've defined the variable "meal" here to the name of the food we ate for breakfast!
+meal = "An English Muffin!"
+
+# Printing out breakfast
+print("Breakfast:")
+print(meal)
+
+# Now update meal to be lunch!
+meal = "A Philly CheeseSteak Sandwich With A Bag Of Chips!"
+
+# Printing out lunch
+print("Lunch:")
+print(meal)
+
+# Now update "meal" to be dinner
+meal = "Fried Chicken, Mashed Potatoes, Corn On The Cob With Apple Pie A La Mode For Dessert!"
+# Printing out dinner
+print("Dinner:")
+print(meal)
+
+print("")
